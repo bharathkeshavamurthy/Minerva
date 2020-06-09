02f20d6a2e2f143ba968ff7b8fca780212fa3f84
Bharath Keshavamurthy <bkeshava@purdue.edu>
2020-06-09 19:28:17 -0400

    Active Incumbent [Dev]

--- src/main/org/purdue/ece/minerva/DARPASC2ActiveIncumbentAnalysis.py
+++ src/main/org/purdue/ece/minerva/DARPASC2ActiveIncumbentAnalysis.py
@@ -5,9 +5,13 @@
 # Organization: School of Electrical and Computer Engineering, Purdue University, West Lafayette, IN.
 # Copyright (c) 2020. All Rights Reserved.
 
+# Note that in this analysis, BAM Wireless (without the PERSEUS MAC) will be treated as a competitor (or incumbent).
+
 # The imports
+import numpy
 import sqlite3
 import contextlib
+from collections import OrderedDict
 
 
 # The purpose of this class is analyze the occupancy behavior of the DARPA SC2 Active Incumbent and other competitors
@@ -25,7 +29,7 @@ class DARPASC2ActiveIncumbentAnalysis(object):
     SRN_DATA_ACQUISITION_QUERY = 'SELECT DISTINCT srnID as d_srnID FROM Start ORDER BY d_srnID ASC'
 
     # PSD data acquisition query
-    PSD_DATA_ACQUISITION_QUERY = 'SELECT time_ns, '
+    PSD_DATA_ACQUISITION_QUERY = 'SELECT time_ns, psd FROM PSDUpdateEvent WHERE srnID={} ORDER BY time_ns ASC'
 
     # The initialization sequence
     def __init__(self, db_file):
@@ -36,7 +40,8 @@ class DARPASC2ActiveIncumbentAnalysis(object):
         # Scenario end time in nanoseconds
         self.simulation_stop_time = self.execute(self.STOP_TIME_ACQUISITION_QUERY)[0]
         # Identifiers for all the Standard Radio Nodes (SRNs) in the BAM_Wireless network--deployed in this scenario
-        self.srn_ids = [x[0] for x in self.execute(self.SRN_DATA_ACQUISITION_QUERY)]
+        self.srns = [x[0] for x in self.execute(self.SRN_DATA_ACQUISITION_QUERY)]
+        # The initialization process has been completed
 
     # Independent, safe, and compact SQLite3 connection instance creation, query execution, and connection closure
     def execute(self, query):
@@ -45,18 +50,49 @@ class DARPASC2ActiveIncumbentAnalysis(object):
                 with contextlib.closing(db_connection.cursor()) as db_cursor:
                     db_cursor.execute(query)
                     return db_cursor.fetchall()
-
-    # Get the IDs of the SRNs in the BAM_Wireless network
-    def get_srn_data(self):
-        return
+        # Nothing to return...
 
     # Get the local PSD data of the SRNs of the BAM_Wireless network
     def get_psd_data(self):
-        db_data = self.execute()
+        # The output to be returned
+        psds = OrderedDict()
+        # The transient global PSD data variable
+        global_psd_data = {n: {} for n in self.srns}
+        # Unfiltered PSD data extraction
+        for srn in self.srns:
+            srn_psd_data = self.execute(self.PSD_DATA_ACQUISITION_QUERY.format(srn))
+            for entry in srn_psd_data:
+                global_psd_data[srn][entry[0]] = 10*numpy.log10(numpy.frombuffer(entry[1], dtype='<f4'))
+        # Filtering, Processing, and Consolidating the extracted PSD data
+        index_srn = max(global_psd_data.keys(), key=lambda x: min(global_psd_data[x].keys()))
+        index_timestamps = sorted(global_psd_data[index_srn].keys())
+        for timestamp in index_timestamps:
+            psds[timestamp] = global_psd_data[index_srn][timestamp]
+            for srn in filter(lambda x: x != index_srn, self.srns):
+                closest_timestamp = min(global_psd_data[srn].keys(), key=lambda x: abs(timestamp - x))
+                psds[timestamp] += global_psd_data[srn][closest_timestamp]
+            psds[timestamp] /= len(self.srns)
+        # Return the processed PSD data
+        return psds
+
+    # After processing the PSD data, determine the occupancy behavior of the incumbents and the competitors in the
+    #   network--during the emulation of the DARPA SC2 Active Incumbent scenario
+    def get_occupancy_behavior(self):
+        # The output to be returned
+        occupancy_behavior_collection = OrderedDict()
+        psds = self.get_psd_data()
+        for timestamp, avg_psd_values in psds.items():
+            # The discretization remains constant throughout (1024)
+            for channel in range(len(avg_psd_values)):
+                occupancy_behavior_collection.setdefault(channel, []).append(
+                    (lambda: 0, lambda: 1)[int(avg_psd_values[channel]) >= -20]())
+        # Return the extracted occupancy behavior collection
+        return occupancy_behavior_collection
 
     # The termination sequence
     def __exit__(self, exc_type, exc_val, exc_tb):
         print('[INFO] DARPASC2ActiveIncumbentAnalysis Termination: Tearing things down...')
+        # Nothing to do here...
 
 
 # Run Trigger
@@ -66,5 +102,6 @@ if __name__ == '__main__':
     print('[INFO] DARPASC2ActiveIncumbentAnalysis main: Starting the analysis of the occupancy behavior of the Active '
           'Incumbent and our competitors in a DARPA SC2 Active Incumbent scenario...')
     analyser = DARPASC2ActiveIncumbentAnalysis(db)
+    _occupancy_behavior_collection = analyser.get_occupancy_behavior()
     print('[INFO] DARPASC2ActiveIncumbentAnalysis main: Completed the analysis of the occupancy behavior of the Active '
           'Incumbent and our competitors in a DARPA SC2 Active Incumbent scenario!')
